bd4551734d8b64d60340c4f78e488e72f51945ad
niyas <hojaniyazhan@yahoo.com>
2021-09-05 22:46:47 +0200

    adding to remote repository

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
+
+
+const Header = () => {
+  return (
+    <header>
+      <Container>
+        <Row>
+          <Col>
+            Copyright &copy; 2021 Eshop
+          </Col>
+        </Row>
+      </Container>
+    </header>
+  )
+}
+
+export default Header
