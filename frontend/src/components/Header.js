20cec0aa94f3ed27c32510b4a57e45d5de4614b1
niyas <hojaniyazhan@yahoo.com>
2021-09-07 12:15:58 +0200

    Ending of sample frontend and beginng of the backend

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,33 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap';
-
+import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+// import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
-      <Container>
-        <Row>
-          <Col>
-            Copyright &copy; 2021 Eshop
-          </Col>
-        </Row>
-      </Container>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+        <LinkContainer to='/'>
+            <Navbar.Brand>ProShop</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i> Sign In
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   )
 }
